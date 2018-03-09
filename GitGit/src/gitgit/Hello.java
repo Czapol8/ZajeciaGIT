341f8b29db0b2b250d0fd7417fc4f91fc6a9c62a
Mateusz <mateusz.janota@gmail.com>
2018-03-09 12:41:43 +0100

    Add Hello class with basic implementation

---
+++ GitGit/src/gitgit/Hello.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitgit;
+
+/**
+ *
+ * @author Czapol8
+ */
+public class Hello {
+
+	private String helloMessage;
+
+	public Hello() {
+		helloMessage = "Hello World!";
+	}
+
+	public String sayHello()
+	{
+		return helloMessage;
+	}
+	
+}
